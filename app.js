dea5c83c99b8a80d235b10fb1f23fd29cc0b703d
Brijbhan <brijphy@gmail.com>
2018-08-19 14:18:14 +0530

    as

--- app.js
+++ app.js
@@ -15,42 +15,124 @@
 // }
 
 
+/**
+ * 
+ *      for me its is best api for zip code  
+ * 
+ * http://maps.googleapis.com/maps/api/geocode/json?address=201307&sensor=true
+ */
+
+
+
 // creating single obje
 
-$(
+//$(
 
-    ()=>{
+//     ()=>{
 
-        $.getJSON(
-            'http://api.flickr.com/services/feeds/photos_public.gne?id=32612727@N07&lang=en-us&format=json&jsoncallback=?'
-            ,{
-                //optional settings 
-            }
-        ).done(
-            (res)=>{
-                i = 0 
-                console.log(res);
-                res.items.forEach(e => {
+//         $.getJSON(
+//             'http://maps.googleapis.com/maps/api/geocode/json?address=201307&sensor=true'
+//             ,{
+//                 //optional settings 
+//             }
+//         ).done(
+//             (res)=>{
+//                 i = 0 
+//                 console.log(res);
+//                 res.items.forEach(e => {
                     
-                    console.log( e.media.m);
-                    li = document.createElement('li');
-                    img = document.createElement('img');
-                    li.appendChild(img);
-                    document.querySelector('.img').appendChild(li);
-                    $('img')[i].src = e.media.m;
-                    i++;
-                });
-                // here success code
+//                     console.log( e.media.m);
+//                     li = document.createElement('li');
+//                     img = document.createElement('img');
+//                     li.appendChild(img);
+//                     document.querySelector('.img').appendChild(li);
+//                     $('img')[i].src = e.media.m;
+//                     i++;
+//                 });
+//                 // here success code
+//             }
+
+//         ).fail(
+//             (failres)=>{
+//                 console.log(failres);
+//                 console.log('here something going wrong');
+//                  // some failed code   
+//             }
+//         )
+
+//     }
+
+// )
+
+
+
+/**
+ * 
+ *  Google Zip code Api Test 
+ * 
+ * 
+ */
+
+$(
+    ()=>{
+        $("#zip").change(
+            (eve)=>{
+                let url = 'http://maps.googleapis.com/maps/api/geocode/json?address='+$('#zip').val()+'&sensor=true';
+                console.log(url);
+                $.getJSON(
+                    url,
+                    {
+
+                    }
+                ).done((res)=>{
+                    console.log(res);
+                    if (res.results.length>0){ 
+                        $('span').css('display', 'none');   
+                        let add =  res.results[0].address_components;
+                        console.log(add.status);
+                        add.forEach(add => {
+
+                            add.types.forEach((type)=>{
+                                if (type == 'administrative_area_level_2'){
+                                    $('#ct').val(add.long_name);
+                                }
+                                if (type == 'administrative_area_level_1'){
+                                    $('#st').val(add.long_name);
+
+                                } 
+                                if (type == 'country' ){
+                                    $('#con').val(add.long_name);
+                                }
+
+                            })
+
+
+                        });
+                    } else {
+                        $('span').css('display','inline');    
+                        $('#ct').val('');
+                    
+                    
+                        $('#st').val('');
+
+                    
+                    
+                        $('#con').val('');
+
+
+                    }
+
+                    
+        
+                }).fail((res)=>{
+                    console.log(res);
+                    
+                    console.log('net Error');
+
+                })
+
             }
 
-        ).fail(
-            (failres)=>{
-                console.log(failres);
-                console.log('here something going wrong');
-                 // some failed code   
-            }
         )
-
     }
-
-)
+);
